efa0f3ed9891eb451db8036f1bc9c7a2d283e92d
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-19 13:08:45 +0530

    Add array rotation functionality in Necklace.cpp
    Implement rotation of an array based on user input.

---
+++ Necklace.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--)
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    k = k%n;
+    vector<int> b;
+    for(int i = k;i<n;i++)
+    {
+        b.push_back(a[i]);
+    }
+    for(int i=0;i<k;i++)
+    {
+        b.push_back(a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+}
+}
